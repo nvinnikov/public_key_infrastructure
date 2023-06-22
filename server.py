bbb25b8120a044628c02fe40195d9e3b31b31bac
nvinnikov <88853518+nvinnikov@users.noreply.github.com>
2023-06-22 14:48:12 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import subprocess
+from flask import Flask
+
+app = Flask(__name__)
+
+def generate_ssl_keys():
+    # Генерация закрытого ключа
+    subprocess.run(['openssl', 'genpkey', '-algorithm', 'RSA', '-out', 'key.pem'])
+
+    # Создание самоподписанного сертификата с расширением subjectAltName
+    subprocess.run(['openssl', 'req', '-new', '-x509', '-sha256', '-key', 'key.pem', '-out', 'cert.pem', '-days', '365', '-subj', '/CN=localhost', '-extensions', 'v3_req', '-config', 'openssl.cnf'])
+
+# Генерация SSL-ключей перед запуском сервера
+generate_ssl_keys()
+
+@app.route('/')
+def hello():
+    return "Hello, secure world!"
+
+if __name__ == '__main__':
+    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
