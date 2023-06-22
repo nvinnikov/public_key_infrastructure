42395692702068954cdae06e72ac69a70524e878
nvinnikov <88853518+nvinnikov@users.noreply.github.com>
2023-06-22 22:51:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,11 +11,12 @@ def generate_ssl_keys():
     subprocess.run(['openssl', 'req', '-new', '-x509', '-sha256', '-key', 'key.pem', '-out', 'cert.pem', '-days', '365', '-subj', '/CN=localhost', '-extensions', 'v3_req', '-config', 'openssl.cnf'])
 
 # Генерация SSL-ключей перед запуском сервера
-generate_ssl_keys()
+#generate_ssl_keys()
 
 @app.route('/')
 def hello():
     return "Hello, secure world!"
 
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
+    app.run(ssl_context=('certificate.crt', 'private.key'), debug=True)
+
